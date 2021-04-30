975073b0496899ff394f63de1324a5a2fc07495c
seitaroh <seitaro0206@gmail.com>
2021-04-30 22:52:09 +0900

    URLパターンの変更

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include(upload_router.urls)),
-    url('', TemplateView.as_view(template_name='index.html'))
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
